8c168261fd7075055905b8d36b54c258c6fe2497
tsyw <1028472974@qq.com>
2016-06-27 23:44:37 +0800

    ch2 updated
    2-11 and 2-12

---
+++ ch2/2_11.cc
@@ -0,0 +1,3 @@
+//extern int ix = 1024;//definition
+//int iy;//definition
+//extern int iz;//declaration
---
+++ ch2/2_12.cc
@@ -0,0 +1,5 @@
+//int double = 3.14;//illegal
+//int _;//legal
+//int catch-22;//illegal
+//int 1_or_2;//illegal
+//double Double = 3.14;//legal
