86707128528c3cb5fda39d1e0064efdaeb706c6a
tsyw <1028472974@qq.com>
2016-06-27 22:26:22 +0800

    update chapter two
    added answers from 2_1 to 2_9

---
+++ ch2/2_2.cc
@@ -0,0 +1,4 @@
+//rate: double
+//principal: double
+//payment: double
+//float maybe good either, but double is better than float.
---
+++ ch2/2_5.cc
@@ -0,0 +1,4 @@
+//(a)'a': char, L'a': wchar_t, "a": string, L"a": wchar_t string
+//(b)10: signed int, 10u: unsigned int, 10L: long, 10uL: unsigned long, 012: octal digit int, 0xC: int hexadecimal
+//(c)3.14: double, 3.14f: float, 3.14L: long double
+//(d)10: int, 10u: unsigned int, 10.: double, 10e-2: double
