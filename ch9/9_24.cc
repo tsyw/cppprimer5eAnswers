35287cc3cfc20d8a1105d790650aabefa27d88b2
tsyw <1028472974@qq.com>
2016-09-18 22:31:02 +0800

    ex9_24 fixed

--- ch9/9_24.cc
+++ ch9/9_24.cc
@@ -3,9 +3,11 @@
 
 int main()
 {
-    std::vector<int> ivec = {1};
-    std::cout << ivec[0] << " " << *ivec.begin() << " " 
-              << ivec.front() << " " << ivec.at(0) << std::endl;
+    std::vector<int> ivec;
+    std::cout << ivec[0] << std::endl;
+    std::cout << *ivec.begin() << std::endl;
+    std::cout << ivec.front() << std::endl;
+    std::cout << ivec.at(0) << std::endl;
 
     return 0;
 }
